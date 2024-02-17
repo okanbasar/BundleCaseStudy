482e3871cfd623285a168ee83196202778fc4a7a
Okan Engin Basar <okanbasar@yahoo.com>
2024-02-17 22:28:35 +0300

    Case 2 - MQ->SQL consumer app is done!

---
+++ src/main/java/app/bundle/casestudy/casetwo/ActiveMQToSQLApp.java
@@ -0,0 +1,49 @@
+package app.bundle.casestudy.casetwo;
+
+import app.bundle.casestudy.casetwo.connectors.ActiveMQConsumer;
+import app.bundle.casestudy.casetwo.models.RandomData;
+import jakarta.jms.Message;
+import jakarta.jms.MessageConsumer;
+import jakarta.jms.TextMessage;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import static app.bundle.casestudy.casetwo.constants.Constants.*;
+
+public class ActiveMQToSQLApp {
+
+    public static void main(String[] args) {
+        try {
+            // Connect to MQ and create consumer
+            MessageConsumer consumer = new ActiveMQConsumer().getMessageConsumer();
+
+            // Connect to SQL server
+            java.sql.Connection dbConnection = DriverManager.getConnection(SQL_DB_URL, SQL_DB_USER, SQL_DB_PASSWORD);
+
+            // Listen MQ and write to MongoDB collection
+            while (true) {
+                Message message = consumer.receive();
+                if (message instanceof TextMessage textMessage) {
+                    String data = textMessage.getText();
+                    RandomData randomData = new RandomData(data);
+
+                    System.out.println("Data is inserted to SQL table: " + randomData);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertDataIntoSQLDatabase(java.sql.Connection connection, RandomData randomData) throws Exception {
+        // Before inserting the data, make sure DB is installed, can be connected and the table SQL_DB_TABLENAME is created.
+        String sql = "INSERT INTO " + SQL_DB_TABLENAME + " (TIMESTAMP,RANDOM,HASH) VALUES (?,?,?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, randomData.getTimestamp());
+        statement.setInt(2, randomData.getRandom());
+        statement.setString(3, randomData.getHash());
+        statement.executeUpdate();
+        statement.close();
+    }
+}
---
+++ src/main/java/app/bundle/casestudy/casetwo/connectors/ActiveMQConsumer.java
@@ -0,0 +1,51 @@
+package app.bundle.casestudy.casetwo.connectors;
+
+import jakarta.jms.*;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import static app.bundle.casestudy.casetwo.constants.Constants.ACTIVE_MQ_BROKER_URL;
+import static app.bundle.casestudy.casetwo.constants.Constants.ACTIVE_MQ_QUEUE_NAME;
+
+public class ActiveMQConsumer {
+
+    private static MessageConsumer messageConsumer;
+    private static Session session;
+    private static Connection connection;
+
+    public MessageConsumer getMessageConsumer() {
+        return messageConsumer;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void closeConnection() {
+        try {
+            session.close();
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            connection.close();
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ActiveMQConsumer() {
+        try {
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ACTIVE_MQ_BROKER_URL);
+            connection = connectionFactory.createConnection();
+            connection.start();
+
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue queue = session.createQueue(ACTIVE_MQ_QUEUE_NAME);
+
+            messageConsumer = session.createConsumer(queue);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
